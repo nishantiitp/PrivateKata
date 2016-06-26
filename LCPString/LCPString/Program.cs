5d395f412b27a016eb9f3508f926001c168b730a
nishant <nishant.iitp@gmail.com>
2016-06-26 13:21:49 +0530

    Least Common Prefix of a string

---
+++ LCPString/LCPString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace LCPString
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string prefix = GetStringsLCP (new string[] { "abcdef", "abcedfrdf", "abcdaer", "sdfadsf", "sdfadf" }, 0, 4);
+			Console.WriteLine ("Hello World!");
+		}
+
+		private static string GetStringsLCP (string[] input, int i, int j)
+		{
+			if (j - i == 1) {
+				return GetStringLCP (input [i], input [j]);
+			} else if (i == j) {
+				return input [i];
+			} else {
+				return GetStringLCP (GetStringsLCP (input, i, (i + j) / 2), GetStringsLCP (input, (i + j) / 2 + 1, j));
+			}
+		}
+
+		private static string GetStringLCP (string a, string b)
+		{
+			List<char> lcp = new List<char> ();
+			int i = 0;
+			while (i < a.Length && i < b.Length) {
+				if (a [i] == b [i]) {
+					lcp.Add (a [i]);
+				} else {
+					return new string (lcp.ToArray ());
+				}
+				i++;
+			}
+
+			return new string (lcp.ToArray ());
+		}
+	}
+}
---
+++ LexicographicStringRank/LexicographicStringRank/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace LexicographicStringRank
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string input = Console.ReadLine ();
+			char[] sortedInput = input.ToCharArray ();
+			Array.Sort (sortedInput); 		
+			int val = GetLexicographicRank (input, sortedInput.OfType<char> ().ToList ());
+			Console.WriteLine ("Hello World!");
+		}
+
+		private static int GetLexicographicRank (string input, List<char> sortedInput)
+		{
+			int i = 0;
+			int earlierPermutations = 0;
+			int stringStartingWithEachChar = 0;
+			while (i < input.Length) {
+				stringStartingWithEachChar = GetFactorial (sortedInput.Count) / (sortedInput.Count);
+				int getPositionInSortedArray = sortedInput.FindIndex (item => item == input [i]);
+				if (getPositionInSortedArray == 0) {
+					getPositionInSortedArray = 1;
+				}
+
+				sortedInput.Remove (input [i]);
+				earlierPermutations += stringStartingWithEachChar * getPositionInSortedArray;
+				i++;
+			}
+
+			return earlierPermutations; 
+		}
+
+		private static int GetFactorial (int n)
+		{
+			if (n == 1 || n == 0)
+				return 1;
+			return n * GetFactorial (n - 1);
+		}
+	}
+}
